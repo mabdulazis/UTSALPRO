09ae053e35b329ea7818ff1bc6c0b46300236b00
mabdulazis <115127684+mabdulazis@users.noreply.github.com>
2022-11-23 22:05:39 -0700

    Update and rename README.md to Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        String salam = "Assalammualaikum ^^";
+        String nama = "Muhammad abdul azis";
+        int tahun = 2003;
+
+        System.out.println(salam);
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Saya lahir di tahun  : " + tahun);
+    }
+}
